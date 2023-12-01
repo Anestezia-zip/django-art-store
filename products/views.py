699f9f47cb2f321077517245ce7d3772d2cddf51
Anestezia-zip <plekatybtc@gmail.com>
2023-12-01 15:24:53 +0000

    Added product detail functionallity

--- products/views.py
+++ products/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
 
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-
     products = Product.objects.all()
 
     context = {
@@ -12,3 +11,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
