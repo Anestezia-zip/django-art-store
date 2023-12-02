aed69eba625fef48b1b1c67e25937cedbbc8217d
Anestezia-zip <plekatybtc@gmail.com>
2023-12-02 11:38:01 +0000

    Added product count to the product page

--- products/views.py
+++ products/views.py
@@ -20,11 +20,11 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
-            if sortkey == 'date':
+            elif sortkey == 'date':  
                 sortkey = 'date_created'
-
-            if 'direction' in request.GET:
+            elif sortkey == 'category':
+                sortkey = 'category__name'
+            elif 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
