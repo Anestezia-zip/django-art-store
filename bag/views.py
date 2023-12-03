9d660371bfaabe09b2fdf614f04f01a5ea2ba05b
Anestezia-zip <plekatybtc@gmail.com>
2023-12-03 15:05:26 +0000

    Added notifications

--- bag/views.py
+++ bag/views.py
@@ -34,9 +34,11 @@ def remove_from_bag(request, item_id):
 
         if item_id in bag:
             bag.pop(item_id)
+        messages.success(request, f'The painting was remover from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
