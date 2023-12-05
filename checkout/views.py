c1feef365f498689b553b494997bb8ed2f4a5165
Anestezia-zip <plekatybtc@gmail.com>
2023-12-05 10:30:52 +0000

    Added stripe elemnts

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OJuvzDLZ8UGOw3ldy81W2T7WRoSaxlMlF4AaatOtz73zi7xlUgDL7xaEdhJsd9d59ys6k3QVi3Yq11qs9jkfBa4003VOoQVkl',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
