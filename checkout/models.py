6cc8c36230e05056aeeda957da878d76298837b6
Anestezia-zip <plekatybtc@gmail.com>
2023-12-10 11:49:39 +0000

    Added product form

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         """
         Generate a random, unique order number using UUID
         """
-        return uuid.uuid4().hex.upper()
+        return str(uuid.uuid4().int)[:6]
 
     def update_total(self):
         """
