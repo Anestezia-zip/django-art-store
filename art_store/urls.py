665d16644b43793e93b66bbb4574d68dfff17c30
Anestezia-zip <plekatybtc@gmail.com>
2023-12-22 12:25:37 +0000

    Created 404 page

--- art_store/urls.py
+++ art_store/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = 'art-store.views.handler404'
