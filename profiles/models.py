e3a85f260c3b139ddb5fb6631f08adfef8f062c3
Anestezia-zip <plekatybtc@gmail.com>
2023-12-22 00:28:48 +0000

    Comented profile view

--- profiles/models.py
+++ profiles/models.py
@@ -34,6 +34,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     # Existing users: just save the profile
-    if created:
-        UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
+    # if created:
+        # UserProfile.objects.create(user=instance)
+    # instance.userprofile.save()
