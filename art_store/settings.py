eac2ccf60468c54cd1e703afea4ed6b7142b94ab
Anestezia-zip <plekatybtc@gmail.com>
2023-12-22 00:18:41 +0000

    Changed debug var

--- art_store/settings.py
+++ art_store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1','artful-urbex-3e13a9b6e83c.herokuapp.com']
 
