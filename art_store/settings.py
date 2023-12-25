f1455754778818431ce15f29ed8df4abb6d5abb6
Anestezia-zip <plekatybtc@gmail.com>
2023-12-25 14:23:38 +0000

    Added about section and favicone

--- art_store/settings.py
+++ art_store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','artful-urbex-3e13a9b6e83c.herokuapp.com']
 
