14aa94bb4bb452bf3c0a1bf8745efe7f6d6543d8
Anestezia-zip <plekatybtc@gmail.com>
2023-12-27 17:16:13 +0000

    Added styling to authentification pages

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('rate_product/', views.rate_product, name='rate_product_generic'),
-    # Первый URL-шаблон с параметром product_id
+    path('<int:product_id>', views.product_detail, name='product_detail'),
     path('rate_product/<int:product_id>/', views.rate_product, name='rate_product'),
 ]
