1a86ab11f73bd396b968b98b32bac400d46380d2
Anestezia-zip <plekatybtc@gmail.com>
2023-12-30 16:33:21 +0000

    Changed profile view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from . import views
-from functools import partial
+# from functools import partial
 
 
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
     
-    # path('edit/<int:painting_id>/', views.edit_painting, name='edit'),
-    # path('delete/<int:painting_id>/', views.delete_painting, name='delete'),
+    path('edit_painting/<int:painting_id>/', views.edit_painting, name='edit_painting'),
+    path('delete_painting/<int:painting_id>/', views.delete_painting, name='delete_painting'),
 
-    path('edit_painting/<int:painting_id>/', partial(views.edit_painting, temporary=False), name='edit_painting'),
-    path('delete_painting/<int:painting_id>/', partial(views.delete_painting, temporary=False), name='delete_painting'),
+    # path('edit_painting/<int:painting_id>/', partial(views.edit_painting, temporary=False), name='edit_painting'),
+    # path('delete_painting/<int:painting_id>/', partial(views.delete_painting, temporary=False), name='delete_painting'),
 ]
